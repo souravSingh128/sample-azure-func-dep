f48b90b0ec77f7f3e951778d0be6c7e19eb2c34c
souravSingh128 <81309000+souravSingh128@users.noreply.github.com>
2023-03-01 15:59:10 +0530

    removed-duplicate

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -10,13 +10,3 @@ module.exports = async function (context, req) {
     body: responseMessage,
   };
 };
-const name = req.query.name || (req.body && req.body.name);
-const responseMessage = name
-  ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-  : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-const val = "jflfljslfjlssjflsjfl";
-
-context.res = {
-  // status: 200, /* Defaults to 200 */
-  body: responseMessage,
-};
