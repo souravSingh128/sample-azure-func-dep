7ad987f2ba2937ed176feddd2fad47a7aa8fff47
souravSingh128 <81309000+souravSingh128@users.noreply.github.com>
2023-03-01 16:54:00 +0530

    edited dev-branch

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -5,6 +5,7 @@ module.exports = async function (context, req) {
     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+  const rand = "some random constant value";
   context.res = {
     // status: 200, /* Defaults to 200 */
     body: responseMessage,
