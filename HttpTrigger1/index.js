bef9398ec0427c0137a4a3328cad4bdd781123f3
souravSingh128 <81309000+souravSingh128@users.noreply.github.com>
2023-03-01 15:33:52 +0530

    added value

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,6 +1,7 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+<<<<<<< Updated upstream
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
@@ -11,3 +12,15 @@ module.exports = async function (context, req) {
         body: responseMessage
     };
 }
+=======
+  const name = req.query.name || (req.body && req.body.name);
+  const responseMessage = name
+    ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  const val = "jflfljslfjlssjflsjfl";
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: responseMessage,
+  };
+};
+>>>>>>> Stashed changes
